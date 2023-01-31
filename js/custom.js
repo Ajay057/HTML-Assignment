6370576eb4e45c1134085b4ebece47a15cb6f40e
Ajay Patel <ajaypatelaj7@gmail.com>
2023-01-31 15:15:35 +0530

    Responsive Design Is Completed

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidBar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "";
+}
+
+function closeNav() {
+    document.getElementById("mySidBar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
